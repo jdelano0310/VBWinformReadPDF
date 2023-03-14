52b1ab982e08a308236150923e8a7e72344baff4
Jon <jd31068@hotmail.com>
2023-03-14 11:12:32 -0400

    Add project files.

---
+++ VBWinformReadPDF/Form1.Designer.vb
@@ -0,0 +1,66 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        OpenFileDialog1 = New OpenFileDialog()
+        Button1 = New Button()
+        ListBox1 = New ListBox()
+        SuspendLayout()
+        ' 
+        ' OpenFileDialog1
+        ' 
+        OpenFileDialog1.FileName = "OpenFileDialog1"' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(1, 2)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(80, 22)
+        Button1.TabIndex = 0
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 15
+        ListBox1.Location = New Point(1, 30)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(672, 139)
+        ListBox1.TabIndex = 1
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(677, 179)
+        Controls.Add(ListBox1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ VBWinformReadPDF/Form1.vb
@@ -0,0 +1,77 @@
+﻿Imports System.IO
+Imports Microsoft.Office.Interop
+Imports Spire.Pdf
+Imports Spire.Pdf.Texts
+Public Class Form1
+    Dim extractedText As String
+    Private Sub extractPDFText()
+        ListBox1.Items.Add("Launching Spire PDF")
+        Dim doc As New PdfDocument()
+
+        'Load a PDF file
+        doc.LoadFromFile("C:\Users\jd310\Documents\DDU Guide_Tutorial_Wagnardsoft.pdf")
+
+        ListBox1.Items.Add("Opened PDF File")
+        'Get the second page
+        Dim page As PdfPageBase = doc.Pages(1)
+
+        'Create a PdfTextExtractot object
+        Dim textExtractor As New PdfTextExtractor(page)
+
+        ListBox1.Items.Add("Page aquired")
+
+        'Create a PdfTextExtractOptions object
+        Dim extractOptions As New PdfTextExtractOptions()
+
+        'Set isExtractAllText to true
+        extractOptions.IsExtractAllText = True
+
+        'Extract text from the page
+        extractedText = textExtractor.ExtractText(extractOptions)
+
+        ListBox1.Items.Add($"Text Extracted - length: {extractedText.Length}")
+
+        doc.Close()
+        doc.Dispose()
+
+    End Sub
+
+    Private Sub writeToExcelFile()
+
+        ListBox1.Items.Add("Launching Excel")
+        Dim xlApp As Excel.Application
+        Dim xlBook As Excel.Workbook
+        Dim xlSheet As Excel.Worksheet
+        xlApp = CreateObject("Excel.Application")
+        xlBook = xlApp.Workbooks.Open("C:\Users\jd310\Documents\Book1.xlsm")
+        xlSheet = xlBook.Sheets(2)
+
+        ListBox1.Items.Add($"Selected {xlSheet.Name}")
+        Try
+            ' Write data to Excel sheet
+            xlSheet.Range("A27").Value = extractedText
+            ListBox1.Items.Add("Placed values in Excel")
+        Catch ex As Exception
+            ListBox1.Items.Add($"Unable to complete, Error: {ex.Message}")
+        End Try
+
+        ' Save and close Excel file
+        ListBox1.Items.Add("Saving closing")
+        xlBook.Save()
+        xlBook.Close()
+        xlApp.Quit()
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        extractPDFText()
+        writeToExcelFile()
+
+        ListBox1.Items.Add("Complete")
+    End Sub
+
+
+
+
+End Class
